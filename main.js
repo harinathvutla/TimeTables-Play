af7598c6294520f6e7aa10181097b34cd5d61826
Harinath Vutla <harinathvutla428@gmail.com>
2019-08-26 23:44:40 +0200

    second commit

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ let point1 = new Points(300, 300, 300);
 let line1 = new Multiplierline();
 
 let multiple = 2;
-
+let flag = 0;
+var hues = 0;
 function setup() {
   let canvas = createCanvas(600, 600);
   canvas.parent("canvas");
@@ -16,18 +17,16 @@ function draw() {
   point1.drawPoints();
   colorMode(HSB);
   strokeWeight(1);
-  let flag = 0;
-  /* if(flag===0)
-  {
-    let hue = 0;
-    hue+=10;
-  } */
+  stroke(hues, 100, 100);
+  if (frameCount % 120 === 0) hues = hues + 30;
+  if (hues === 360) {
+    hues = 0;
+  }
 
-  if (frameCount % 120 === 0) hue = hue + 10;
-  stroke(hue, 204, 100);
+  console.log("hue: " + hues);
   line1.drawLines(multiple);
-  if (frameCount % 12 === 0) {
-    multiple += 0.1;
+  if (frameCount % 30 === 0) {
+    multiple += 1;
     console.log("multiple: " + multiple);
   }
 }
